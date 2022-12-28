dd62a69e41a411e88a65e0941d6bbeee2db761ff
unknown <spoorthis.xworkz@gmail.com>
2022-12-28 20:34:02 +0530

    overrideEquals

---
+++ Collection/src/com/xwrokz/Collection/AirportRunner.java
@@ -0,0 +1,30 @@
+package com.xwrokz.Collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.xwrokz.Collection.dto.AirportDTO;
+
+public class AirportRunner {
+
+	public static void main(String[] args) {
+		
+		AirportDTO airportDTO1 = new AirportDTO("KempegowdaInternationalAirport", true, "Bengaluru");
+		AirportDTO airportDTO2 = new AirportDTO("MagaloreInternationalAirport",false, "Mangalore");
+		AirportDTO airportDTO3 = new AirportDTO("BelgaviAirport",false,"Belagavi");
+		AirportDTO airportDTO4 = new AirportDTO("GulbargaAirport",true,"Kalburagi");
+		AirportDTO airportDTO5 = new AirportDTO("HALairport",false,"Bengaluru");
+		
+		boolean equals = airportDTO1.equals(airportDTO5);
+		System.out.println(equals);
+		
+		Collection<AirportDTO> airports = new ArrayList<AirportDTO>();
+		airports.add(airportDTO1);
+		airports.add(airportDTO2);
+		
+		boolean contains = airports.contains(airportDTO5);
+		System.out.println("contains "+contains);
+		
+	}
+
+}
