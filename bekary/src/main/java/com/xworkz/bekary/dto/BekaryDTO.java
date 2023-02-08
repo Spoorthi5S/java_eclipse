7a3f6de79b9a7b39b18bcbdf8b0cf936e2f9aad8
unknown <spoorthis.xworkz@gmail.com>
2023-02-08 19:17:21 +0530

    dispatcherServlet

---
+++ bekary/src/main/java/com/xworkz/bekary/dto/BekaryDTO.java
@@ -0,0 +1,14 @@
+package com.xworkz.bekary.dto;
+
+import lombok.Data;
+
+@Data
+public class BekaryDTO {
+
+	private String name;
+	private String ownerName;
+	private String wifeName;
+	private boolean married;
+	private String famousFor;
+	private String since;
+}
